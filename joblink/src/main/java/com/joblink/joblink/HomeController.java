0c6520f4e96ac122294bf45c73423c6063acb26f
Tien_nu <nguyenthithuytiensmstan@gmail.com>
2025-09-26 00:14:13 +0700

    Update front-end signin, signup, verification

--- joblink/src/main/java/com/joblink/joblink/HomeController.java
+++ joblink/src/main/java/com/joblink/joblink/HomeController.java
@@ -10,4 +10,17 @@ public class HomeController {
     public String home() {
         return "index";
     }
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+    @GetMapping("/verification")
+    public String verification() {
+        return "verification";
+    }
+
 }
