082714a01e2ffd1e7ee1d046c102fd8cca940eb7
Callidra <hoaibao30101@gmail.com>
2025-10-06 17:03:24 +0700

    update signup với google

--- src/main/java/com/joblink/joblink/auth/model/User.java
+++ src/main/java/com/joblink/joblink/auth/model/User.java
@@ -1,15 +1,12 @@
 package com.joblink.joblink.auth.model;
 
-
 import lombok.Data;
 
 @Data
 public class User {
     private int userId;
     private String email;
+    private String fullName;
     private String role;
-
-
+    private String status;
 }
-
-
